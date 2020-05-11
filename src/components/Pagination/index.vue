a544dea3763963aa3f8b2394ca905a3fcea74ad9
wangbaohong <q1329462887@gmail.com>
2020-05-12 00:42:32 +0800

    1). 理解: 响应式数据对象添加属性与删除属性     2). 排序功能     3). 自定义分页组件: Pagination

---
+++ src/components/Pagination/index.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="pagination">
+    <button :disabled="myCurrentPage === 1">上一页</button>
+    <button v-if="startEnd.start > 1">1</button>
+    <button disabled v-if="startEnd.start > 2">•••</button>
+
+    <!-- 连续页码 -->
+    <button
+      v-for="(item, index) in startEnd.end"
+      :key="index"
+      v-if="item >= startEnd.start"
+      :class="{ active: item === myCurrentPage }"
+    >
+      {{ item }}
+    </button>
+
+    <button disabled v-if="startEnd.end < totalPages - 1">•••</button>
+    <button v-if="startEnd.end < totalPages">{{ totalPages }}</button>
+    <button :disabled="myCurrentPage === totalPages">下一页</button>
+
+    <button style="margin-left: 30px" :disabled="true">
+      共 {{ total }} 条
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pagination",
+  props: {
+    currentPage: {
+      type: Number,
+      default: 1,
+    },
+    pageSize: {
+      type: Number,
+      default: 5,
+    },
+    total: {
+      type: Number,
+      default: 0,
+    },
+    showPageNo: {
+      type: Number,
+      default: 3, //最好为奇数
+    },
+  },
+  data() {
+    return {
+      // 将外部传入的当前也i按作为档期那组件的当前页码初始值
+      myCurrentPage: this.currentPage,
+    };
+  },
+  computed: {
+    // 总页数
+    totalPages() {
+      const { total, pageSize } = this;
+      return Math.ceil(total / pageSize);
+    },
+    // 连续页码的开始和结束页码
+    startEnd() {
+      let start, end;
+      const { myCurrentPage, showPageNo, totalPages } = this;
+      // 计算start
+      start = myCurrentPage - Math.floor(showPageNo / 2);
+      if (start < 1) {
+        start = 1;
+      }
+      // 计算end
+      end = start + showPageNo - 1;
+      if (end > totalPages) {
+        end = totalPages;
+        // 根据连续页码数修正start
+        start = end - showPageNo + 1;
+        if (start < 1) {
+          start = 1;
+        }
+      }
+      return { start, end };
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.pagination {
+  button {
+    margin: 0 5px;
+    background-color: #f4f4f5;
+    color: #606266;
+    outline: none;
+    border-radius: 2px;
+    padding: 0 4px;
+    vertical-align: top;
+    display: inline-block;
+    font-size: 13px;
+    min-width: 35.5px;
+    height: 28px;
+    line-height: 28px;
+    cursor: pointer;
+    box-sizing: border-box;
+    text-align: center;
+    border: 0;
+
+    &[disabled] {
+      color: #c0c4cc;
+      cursor: not-allowed;
+    }
+
+    &.active {
+      cursor: not-allowed;
+      background-color: #409eff;
+      color: #fff;
+    }
+  }
+}
+</style>
